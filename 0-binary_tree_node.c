452d3151ea8604fc3378c5f997d24dd7cd82d622
Yared Asmelash <yaredasmelash00@gmail.com>
2024-03-25 16:30:05 +0300

    update node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new == NULL)
 		return (NULL);
 
-	new->n = value;
+	new->n = valu;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
